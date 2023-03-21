acd3395227051dab351510abd64c69e12f182c6d
Jchicode <qianggeek@gmail.com>
2023-03-21 10:26:36 +0800

    update contract
    Signed-off-by: Jchicode <qianggeek@gmail.com>

--- contract/deploy/00_deploy.js
+++ contract/deploy/00_deploy.js
@@ -1,9 +1,8 @@
-require("hardhat-deploy")
-require("hardhat-deploy-ethers")
+require("hardhat-deploy");
+require("hardhat-deploy-ethers");
 
-
-const private_key = network.config.accounts[0]
-const wallet = new ethers.Wallet(private_key, ethers.provider)
+const private_key = network.config.accounts[0];
+const wallet = new ethers.Wallet(private_key, ethers.provider);
 
 module.exports = async ({ deployments }) => {
   const { deploy } = deployments;
@@ -11,13 +10,9 @@ module.exports = async ({ deployments }) => {
   try {
     await deploy("STCDao", {
       from: wallet.address,
-      // since it's difficult to estimate the gas before f4 address is launched, it's safer to manually set
-      // a large gasLimit. This should be addressed in the following releases.
-      // since Ethereum's legacy transaction format is not supported on FVM, we need to specify
-      // maxPriorityFeePerGas to instruct hardhat to use EIP-1559 tx format
-      log: true,
+      log: true
     });
   } catch (error) {
     console.log(error);
   }
-}
+};
--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,25 +1,30 @@
-require("@nomicfoundation/hardhat-toolbox")
-require("hardhat-deploy")
-require("hardhat-deploy-ethers")
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-deploy");
+require("hardhat-deploy-ethers");
 // require("./tasks")
-require("dotenv").config()
+require("dotenv").config();
 
-const PRIVATE_KEY = process.env.PRIVATE_KEY
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
-  defaultNetwork: "hyperspace",
+  defaultNetwork: "sepolia",
   networks: {
     hyperspace: {
       chainId: 3141,
       url: "https://api.hyperspace.node.glif.io/rpc/v1",
-      accounts: [PRIVATE_KEY],
+      accounts: [PRIVATE_KEY]
     },
+    sepolia: {
+      chainId: 11155111,
+      url: "https://sepolia.infura.io/v3/60a1a58a3e8b4aa58451b9467a52818c",
+      accounts: [PRIVATE_KEY]
+    }
   },
   paths: {
     sources: "./contracts",
     tests: "./test",
     cache: "./cache",
-    artifacts: "./artifacts",
-  },
-}
+    artifacts: "./artifacts"
+  }
+};

d8373b35aa9137eee25a5f75858e7aeb870eb8a5
rovast <rovast@163.com>
2023-07-20 13:56:51 +0800

    Update contract
    Signed-off-by: rovast <rovast@163.com>

--- src/utils/contract/user-hub.ts
+++ src/utils/contract/user-hub.ts
@@ -2,7 +2,7 @@ import { getWritebleContractInstance } from "./web3";
 
 // @ts-ignore
 import ABI from "./user-hub.json?raw";
-const ADDR = "0x5D9D1DC34c46707ebCd4e4473c81Cc5BD69A4c98";
+const ADDR = "0x0B6900b5306b49F6B32113BeeB9BA6c43982ce10";
 
 export async function getUserHubContract() {
   const contract = await getWritebleContractInstance(ADDR, ABI);

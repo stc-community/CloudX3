fcf14273bd7422382b0bd885ea3d8dff36f46461
rovast <rovast@163.com>
2023-09-01 13:58:52 +0800

    Better sync script
    Signed-off-by: rovast <rovast@163.com>

--- scripts/sync-cosmos.mjs
+++ scripts/sync-cosmos.mjs
@@ -1,4 +1,5 @@
 import "zx/globals";
+import fs from "node:fs";
 
 console.log("Clone latest code to temp ...");
 await $`git clone https://github.com/stc-community/iot-depin-protocol.git temp`;
@@ -11,3 +12,48 @@ await $`cp -rf temp/vue/src/composables ./src`;
 
 console.log("Clean temp dir");
 await $`rm -rf temp`;
+
+console.log("Remove stc-community-iot-depin-protocol-client-ts");
+await removeDependency("stc-community-iot-depin-protocol-client-ts");
+await $`pnpm install`;
+
+console.log("Add stc-community-iot-depin-protocol-client-ts");
+await addDependencies(
+  "stc-community-iot-depin-protocol-client-ts",
+  "file:ts-client"
+);
+await $`pnpm install`;
+
+async function removeDependency(packageName) {
+  const packageJson = await readPackageJson();
+  Reflect.deleteProperty(packageJson.dependencies, packageName);
+  await writePackageJson(packageJson);
+}
+
+async function addDependencies(packageName, packageVersion) {
+  const packageJson = await readPackageJson();
+  const deps = packageJson.dependencies;
+  Reflect.set(deps, packageName, packageVersion);
+
+  const sortedKeys = Object.keys(deps).sort();
+  const sortedObj = {};
+  sortedKeys.forEach(key => {
+    sortedObj[key] = deps[key];
+  });
+
+  Reflect.set(packageJson, "dependencies", sortedObj);
+
+  await writePackageJson(packageJson);
+}
+
+async function readPackageJson() {
+  const content = await fs.readFileSync("./package.json");
+  return JSON.parse(content);
+}
+
+async function writePackageJson(packageJson) {
+  await fs.writeFileSync(
+    "./package.json",
+    JSON.stringify(packageJson, null, 2) + "\r\n"
+  );
+}
